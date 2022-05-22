fca3e76ac14a887ff9b7fd1ec059b9c8c8c66dcc
jc <jc+git@jcwlkr.io>
2022-05-22 21:33:40 +1000

    resolving records for test domain

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,23 +3,17 @@ require 'net/dns'
 class MainController < ApplicationController
   def index
 
-
   end
 
   def lookup
     # domain = params[:domain]
-    domain = 'apple.com'
-    puts "I have receievd #{params[:domain]}"
+    # puts "I have recieved #{params[:domain]}"
 
-    $result = 10 + params[:domain].to_i
-    puts $result
-
-    resolve = Net::DNS::Resolver.new
-    mx = resolve.mx(domain)
-
+    domain = 'google.com.au' # temp override for testing
     
-
-    puts "Here we go: "
+    $a = Net::DNS::Resolver.start("google.com.au").answer[0]
+    $mx = Net::DNS::Resolver.start("google.com.au", Net::DNS::MX).answer
+    $ns = Net::DNS::Resolver.start("google.com.au", Net::DNS::NS).answer
 
     redirect_to root_path
   end
